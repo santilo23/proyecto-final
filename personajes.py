ef2dd9ca872504fb559c9f7865d7dc80933430f9
Santi <slongo2021@gmail.com>
2024-09-01 20:44:35 -0300

    Creación del archivo enemigos.py, modificacion del archivo personajes.py

---
+++ personajes.py
@@ -0,0 +1,9 @@
+class Personajes:
+    def __init__(self, nombre, salud, fuerza, defensa, nivel=1):
+        self.nombre = nombre
+        self.salud = salud
+        self.fuerza = fuerza
+        self.defensa = defensa 
+        self.nivel = nivel
+
+        
